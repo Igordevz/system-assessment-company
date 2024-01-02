c5acc3786a00592c3543af61b8c3637584a65441
Igordevz <programadorigor.dev@gmail.com>
2024-01-02 17:00:51 -0300

    create conection in socketsand the creation in refatored

--- src/controllers/Received-chart.ts
+++ src/controllers/Received-chart.ts
@@ -1,24 +1,37 @@
 import { Request, Response } from "express";
 import { Model } from "../models/avaliable";
 import VerifyItems from "../utils/verifyItemDB";
+import { io } from "../server";
 
 export async function Chart(req: Request, res: Response) {
   const { range, pergunta } = req.body;
-
+ 
+  const meses = [
+    "jan", "fev", "mar", "abr", "mai", "jun",
+    "jul", "ago", "set", "out", "nov", "dez"
+  ];
+  
 
   const hoje = new Date();
   const dia = hoje.getDate().toString(); 
+  const mes = meses[hoje.getMonth()];
   // pegar perguntar
   const item:any = await Model.findOne({"company.pergunta": pergunta})
   const getFile = item?.company.findIndex((index:any) => index.pergunta == pergunta)
 
+  const formatDate = `${dia} ${mes}`
+  if(!item){
+    return res.status(401).json({msg: "pergunta não encontrada"})
+  }
   const update = await Model.updateOne({"company.pergunta": pergunta}, {
-    $push: {
-      [`company.${getFile}.history`]: dia,
+    $push: {  
+      [`company.${getFile}.history`]: formatDate,
       [`company.${getFile}.range`]: range,
-
     }
   })
+  if(update){
+    io.emit("admin", await Model.findOne())
+  }
 
   res.json({msg: "Obrigado pelo seu feedback"})
 }
